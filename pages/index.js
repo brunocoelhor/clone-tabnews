e6d575df76c7832b1f8745670e809605c73372ad
Bruno Coelho Rodrigues <brunocoelho.r@gmail.com>
2023-09-11 23:38:58 +0000

    modifica a frase  inicial

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 function Home() {
-    return <h1>Martina, eu amo você. Se você me ama, dá uma risadina</h1>
+    return <h1>Martina, eu amo você. Se você me ama, dá uma risadina! 😍 🥰 😘</h1>
 }
 
 export default Home;
