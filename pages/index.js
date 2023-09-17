404b00ea3bb668e0dfc79b6fbd96ba5ce4de1b92
Bruno Coelho Rodrigues <brunocoelho.r@gmail.com>
2023-09-17 13:25:53 +0000

    add video

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,15 @@
 function Home() {
-    return <h1>Martina, eu amo você. Se você me ama, dá uma risadina! 😍 🥰 😘</h1>
+    return <><h1 align="center">Martina, eu amo você. Se você me ama, dá uma risadina! 😍 🥰 😘</h1> 
+    <div id="carregando" align="center">
+    <iframe src='https://www.youtube.com/embed/LMD6MqwErzc?si=sz719q8vZlcUTXpK&amp;controls=0&autoplay=1'
+        width='380' 
+        height='315'
+        frameBorder='0'
+        allow='autoplay; encrypted-media'
+        allowFullScreen
+        title='video'
+/>
+</div></>
 }
 
 export default Home;
