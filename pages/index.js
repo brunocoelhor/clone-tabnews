3f148d9714759294be4a2eea79d21bb827ca828a
Bruno Coelho Rodrigues <brunocoelho.r@gmail.com>
2023-10-08 15:08:53 +0000

    adiciona scripts `lint:check` e `lint:fix`

--- pages/index.js
+++ pages/index.js
@@ -1,15 +1,26 @@
 function Home() {
-    return <><h1 align="center">Martina, eu amo você. Se você me ama, dá uma risadina! 😍 🥰 😘</h1> 
-    <div id="carregando" align="center">
-    <iframe src='https://www.youtube.com/embed/LMD6MqwErzc?si=sz719q8vZlcUTXpK&amp;controls=0&autoplay=1'
-        width='380' 
-        height='315'
-        frameBorder='0'
-        allow='autoplay; encrypted-media'
-        allowFullScreen
-        title='video'
-/>
-</div></>
+  return (
+    <>
+      <h1 align="center">
+        Martina, eu amo você. Se você me ama, dá uma risadina! 😍 🥰 😘
+      </h1>
+      <div id="carregando" align="center">
+        <iframe
+          src="https://www.youtube.com/embed/LMD6MqwErzc?si=sz719q8vZlcUTXpK&amp;controls=0&autoplay=1"
+          width="380"
+          height="315"
+          frameBorder="0"
+          allow="autoplay; encrypted-media"
+          allowFullScreen
+          title="video"
+        />
+      </div>
+    </>
+  );
+}
+
+function teste() {
+  console.log("teste");
 }
 
 export default Home;
